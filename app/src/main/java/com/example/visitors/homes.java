ab26758efe3e4960db6e6285aa105693c4d7a873
premsagar kushwaha <premsagarkushwaha58@gmail.com>
2021-11-08 16:48:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/visitors/homes.java
@@ -0,0 +1,11 @@
+package com.example.visitors;
+
+import android.annotation.SuppressLint;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+public interface homes {
+    @SuppressLint("RestrictedApi")
+    boolean onOptionsItemSlected(@NonNull MenuItem item);
+}
